78bf137928cad2aeb638faf13727764ce072faaa
20PW14 <karthickprnv@gmail.com>
2022-06-04 17:34:47 +0530

    fix fontsizing and position

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
     "Practice keyboarding.",
-    "Flex how good you are at it."
+    "Flex how good you're at it."
 );
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
