427398c85df10d1ece8a5337ae114af29691eb16
20PW14 <karthickprnv@gmail.com>
2022-06-05 21:17:01 +0530

    complete random para in Practice page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const fetch = require('node-fetch');
 
 const PORT = 3000;
 //Middelware
@@ -21,8 +22,24 @@ app.get('/about', (req, res) => {
     res.render('pages/about');
 })
 
-app.get('/practice', (req, res) => {
-    res.render('pages/practice');
+app.get('/practice', async (req, res) => {
+    // GET request to the API endpoint using the fetch.
+    // apiList = [`https://free-quotes-api.herokuapp.com/`, `https://animechan.vercel.app/api/random`, `https://programming-quotes-api.herokuapp.com/Quotes/random`, `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single`];
+    // Did not include the above list because the structure of the json file is different and have to figure it out.
+    apiList = [`https://free-quotes-api.herokuapp.com/`];
+    const apiURL = apiList[Math.floor(Math.random() * apiList.length)];
+    try {
+        await fetch(apiURL)
+            .then(res => res.json())
+            .then(data => {
+                const para = data.quote;
+                res.render('pages/practice', {
+                    para
+                })
+            });
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 app.get('/race', (req, res) => {
