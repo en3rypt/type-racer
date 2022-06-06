3a8740ee1927be392a5c2998189225d635fdd5bc
en3rypt <sanjithnathan@gmail.com>
2022-06-06 17:23:09 +0530

    socket io progress

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 const cors = require('cors');
+const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const fetch = require('node-fetch');
 
-const PORT = 3000;
+
+
+
+
+
+// const cors = require('cors')
+
+
+
+//scoket connection
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('progress', (data) => {
+        console.log(data);
+    });
+    socket.on("hello", (arg) => {
+        console.log(arg); // world
+    });
+});
+
+
 //Middelware
 app.use(bodyParser.json());
 app.use(cors());
@@ -42,10 +68,24 @@ app.get('/practice', async (req, res) => {
     }
 })
 
-app.get('/race', (req, res) => {
-    res.render('pages/race')
+app.get('/race', async (req, res) => {
+    apiList = [`https://free-quotes-api.herokuapp.com/`];
+    const apiURL = apiList[Math.floor(Math.random() * apiList.length)];
+    try {
+        await fetch(apiURL)
+            .then(res => res.json())
+            .then(data => {
+                const para = data.quote;
+                res.render('pages/race', {
+                    para
+                })
+            });
+    } catch (err) {
+        console.log(err);
+    }
+    //res.render('pages/race')
 })
 //start server
-app.listen(process.env.PORT || PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 });
