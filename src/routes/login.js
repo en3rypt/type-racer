615a0c0e7814ab407c3b3a0f782af5500b1c0761
20PW14 <karthickprnv@gmail.com>
2022-10-12 01:12:11 +0530

    complete login and signup db conn

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,12 +1,36 @@
 const login = require('express').Router();
+const { usersdb, q } = require('../../db');
 const fetch = require('node-fetch');
 
 login.get('/', async (req, res) => {
-    res.render('pages/login');
+    res.render('pages/login', { error: '' });
 })
 
-login.post('/', (req, res) => {
+login.post('/', async (req, res) => {
     const { username, password } = req.body;
-    console.log(username, password);
+    try {
+        const users = await usersdb.query(
+            q.Paginate(q.Match(q.Index('username_by_user'))),
+        );
+        if (users.data.includes(username)) {
+            const user = await usersdb.query(q.Map(
+                q.Paginate(
+                    q.Match(q.Index("user_by_username"), username)
+                ),
+                q.Lambda("X", q.Get(q.Var("X")))
+            )
+            );
+            if (user.data[0].data.password === password) {
+                res.render('pages/login', { error: 'User can be logged in.' });
+            } else {
+                res.render('pages/login', { error: 'Wrong password.' });
+            }
+        } else {
+            res.render('pages/login', { error: 'Username does not exist.' });
+        }
+    }
+    catch (e) {
+        console.log({ error: e.message });
+    }
 })
 module.exports = login;
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,18 +1,56 @@
 const signup = require('express').Router();
+const { usersdb, q } = require('../../db');
 const fetch = require('node-fetch');
 const sendEmail = require('../utils/email');
 const { v4: uuidv4 } = require('uuid');
 
+const getuserCount = async () => {
+    try {
+        const getuserCount = await usersdb.query(q.Count(
+            q.Match(q.Index("all_users"))
+        ))
+        return getuserCount
+    } catch (e) {
+        console.log({ error: e.message });
+    }
+};
+
 
 signup.get('/', async (req, res) => {
-    res.render('pages/signup');
+    res.render('pages/signup', { error: '' });
 })
 signup.post('/', async (req, res) => {
-
     const { username, email, password, confirmPassword } = req.body;
-    console.log(username, email, password, confirmPassword);
-
-
-})
+    if (password !== confirmPassword) {
+        res.render('pages/signup', { error: 'Passwords do not match' });
+    } else {
+        try {
+            const users = await usersdb.query(
+                q.Paginate(q.Match(q.Index('username_by_user'))),
+            );
+            if (users.data.includes(username)) {
+                res.render('pages/signup', { error: 'Username already exists' });
+            } else {
+                const createdUser = await usersdb.query(
+                    q.Create(
+                        q.Ref(q.Collection('users'), await getuserCount() + 1),
+                        {
+                            data: {
+                                username: username,
+                                email: email,
+                                password: password,
+                            }
+                        }
+                    )
+                );
+                // console.log(createdUser);
+                res.render('pages/signup', { error: 'User created' });
+            }
+        }
+        catch (e) {
+            console.log({ error: e.message });
+        }
+    }
+});
 
 module.exports = signup;
