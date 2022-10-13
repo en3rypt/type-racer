ed0dabde5e59cd5936a5cd1e4ba74c53e3901630
20PW14 <karthickprnv@gmail.com>
2022-10-13 07:47:10 +0530

    rank for new accounts fix

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -12,6 +12,10 @@ profile.get('/:username', requireAuth, async (req, res) => {
         const gameCount = await typiodb.query(
             q.Count(q.Match(q.Index('games_by_username'), username))
         )
+        const sortedScore = await usersdb.query(
+            q.Paginate(q.Match(q.Index('username_sort_by_score_desc'))),
+        )
+        const rank = sortedScore.data.findIndex((user) => user[1] === username) + 1;
         if (gameCount) {
             const highestWPM = await typiodb.query(
                 q.Max(q.Paginate(q.Match(q.Index('wpm_by_username'), username)))
@@ -19,14 +23,10 @@ profile.get('/:username', requireAuth, async (req, res) => {
             const averageWPM = await typiodb.query(
                 q.Mean(q.Paginate(q.Match(q.Index('wpm_by_username'), username)))
             )
-            const sortedScore = await usersdb.query(
-                q.Paginate(q.Match(q.Index('username_sort_by_score_desc'))),
-            )
-            const rank = sortedScore.data.findIndex((user) => user[1] === username) + 1;
             res.render('pages/profile', { user: user.data, gameCount: gameCount, highestWPM: highestWPM.data, averageWPM: averageWPM.data, rank: rank });
         }
         else {
-            res.render('pages/profile', { user: user.data, gameCount: gameCount, highestWPM: 0, averageWPM: 0, rank: "-" });
+            res.render('pages/profile', { user: user.data, gameCount: gameCount, highestWPM: 0, averageWPM: 0, rank: rank });
         }
     } catch (e) {
         console.log({ error: e });
